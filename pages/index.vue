aa1169d980fd11f140dffb457d8b5d54ede285c0
Ilya Posazhennikov <pos_ilya@vk.com>
2024-01-03 03:45:34 +0500

    Вывод всех сообщений из файла в консоль

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <Tutorial />
+  <input id="file" type="file">
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  mounted () {
+    document.getElementById('file').addEventListener('change', (event) => {
+      const file = event.target.files[0]
+      const reader = new FileReader()
+
+      reader.readAsText(file)
+
+      reader.onload = () => {
+        console.log(JSON.parse(reader.result).chats.list)
+      }
+      reader.onerror = () => {
+        console.log(reader.error)
+      }
+    })
+  }
 }
 </script>
