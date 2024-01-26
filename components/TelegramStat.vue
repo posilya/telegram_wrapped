7c2b847eeca9e7ffb7ad3902bbfcc039a18d4919
Ilya Posazhennikov <pos_ilya@vk.com>
2024-01-26 23:32:25 +0500

    Statistics for all years

--- components/TelegramStat.vue
+++ components/TelegramStat.vue
@@ -3,18 +3,24 @@
     <p v-if="!stat || !year">
       Что-то пошло не так…
     </p>
-    <p v-else-if="stat.chats.length === 0">
-      За {{ year }} год не было сообщений
+    <p v-else-if="stat.numberOfChats === 0">
+      В {{ year }} году не было сообщений
     </p>
     <div v-else>
-      <h2>Ваш топ контактов в {{ year }}</h2>
-      <div class="top-contacts">
-        <div v-for="(chat, index) in stat.chats" :key="index" class="line">
-          <div class="rank">
-            {{ index + 1 }}
+      <div class="top-chats">
+        <h2><span v-if="year == -1">За всё время</span><span v-else>В {{ year }} году</span> вы общались с {{ stat.numberOfChats }} {{ declensionWordsFromNum(stat.numberOfChats, ['человеком', 'людьми', 'людьми'])}}. Вот те, с кем вы переписывались чаще всего:</h2>
+        <div class="top-contacts">
+          <div v-for="(chat, index) in stat.chats" :key="index" class="line">
+            <div class="rank">
+              {{ index + 1 }}
+            </div>
+            <p v-if="!chat.name">
+              Удалённый аккаунт
+            </p>
+            <p v-else>
+              {{ chat.name }}
+            </p>
           </div>
-          <p v-if="!chat.name">Удалённый аккаунт</p>
-          <p v-else>{{ chat.name }}</p>
         </div>
       </div>
     </div>
@@ -32,6 +38,22 @@ export default {
     year: {
       type: Number,
       default: null
+    }
+  },
+  methods: {
+    declensionWordsFromNum (number, words) {
+      number = Math.abs(number) % 100
+      const n = number % 10
+      if (number > 10 && number < 20) {
+        return words[2]
+      }
+      if (n > 1 && n < 5) {
+        return words[1]
+      }
+      if (n === 1) {
+        return words[0]
+      }
+      return words[2]
     }
   }
 }
