d40255305cd50ecbe6d1df174ce2cacf8d59014b
Ilya Posazhennikov <pos_ilya@vk.com>
2024-01-27 22:05:30 +0500

    Show statistics on the number of messages

--- components/TelegramStat.vue
+++ components/TelegramStat.vue
@@ -27,6 +27,11 @@
         <h2>Первое сообщение<span v-if="year != -1"> в {{ year }} году</span> было <span v-if="stat.firstMessage.destination[0] == 'unknown'">в чате с</span><span v-else-if="stat.firstMessage.destination[0] == 'from'">от</span> <span v-else-if="stat.firstMessage.destination[0] == 'to'">для</span> {{ stat.firstMessage.destination[1] }} {{ stat.firstMessage.date }}</h2>
         <p>{{ stat.firstMessage.text }}</p>
       </div>
+      <div class="number-of-messages">
+        <h2><span v-if="stat.myMessages > 0">{{ stat.myMessages }}</span><span v-else>{{ stat.allMessages }}</span> сообщений</h2>
+        <p v-if="stat.myMessages > 0">вы <span v-if="year != -1">отправили в {{ year }} году</span><span v-else>успели отправить за всё время</span>. А получили {{ stat.allMessages - stat.myMessages }}, вместе это {{ stat.allMessages }} сообщений!</p>
+        <p v-else>было <span v-if="year != -1">в {{ year }} году</span><span v-else>за всё ваше время здесь</span>.</p>
+      </div>
       <div v-if="stat.emojis.length > 0" class="most-popular-emoji">
         <h2>{{ stat.emojis[0][0] }}</h2>
         <p>Этот эмодзи вы чаще всего отправляли<span v-if="year != -1"> в {{ year }} году. Наверное, им можно описать весь ваш год</span>.</p>
