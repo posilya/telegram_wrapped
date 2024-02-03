2db44968efc8e6942e94e7959ee0e79778702acc
Ilya Posazhennikov <pos_ilya@vk.com>
2024-02-04 00:10:56 +0500

    Null in the chat name

--- components/stat/block/FirstMessage.vue
+++ components/stat/block/FirstMessage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="first-message">
     <h2>{{ firstMessage.text }}</h2>
-    <p><span v-if="year != -1">Ваш {{ year }} начался</span><span v-else>Ваша история началась</span> {{ firstMessage.date }} с этого сообщения <span v-if="firstMessage.destination[0] == 'unknown'">в чате с</span><span v-else-if="firstMessage.destination[0] == 'from'">от</span><span v-else-if="firstMessage.destination[0] == 'to'">для</span> {{ firstMessage.destination[1] }}.</p>
+    <p><span v-if="year != -1">Ваш {{ year }} начался</span><span v-else>Ваша история началась</span> {{ firstMessage.date }} с этого сообщения <span v-if="firstMessage.destination[0] == 'unknown'">в чате с <span v-if="!firstMessage.destination[1]">удалённым аккаунтом</span></span><span v-else-if="firstMessage.destination[0] == 'from'">от <span v-if="!firstMessage.destination[1]">удалённого аккаунта</span></span><span v-else-if="firstMessage.destination[0] == 'to'">для <span v-if="!firstMessage.destination[1]">удалённого аккаунта</span></span><span v-if="firstMessage.destination[1]"> {{ firstMessage.destination[1] }}</span>.</p>
   </div>
 </template>
 
