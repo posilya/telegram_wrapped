9c69a70f166cd1df12208bac562b36cd0c7deec3
Ilya Posazhennikov <pos_ilya@vk.com>
2024-02-04 17:01:11 +0500

    я ошибка

--- components/stat/block/MostUsedWords.vue
+++ components/stat/block/MostUsedWords.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="words.length > 0" class="most-used-words">
-    <h2>{{ words.slice(0, 10).map(place => place[0]).join(' ') }}</h2>
+    <h2>{{ words.slice(0, 5).map(place => place[0]).join(' ') }}</h2>
     <p>Это предложение из ваших самых частых слов<span v-if="year != -1"> в {{ year }} году</span>.</p>
   </div>
 </template>
